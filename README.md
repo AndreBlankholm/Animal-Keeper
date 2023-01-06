358d6fa556db20eb054ffcc80d978072d4c06b62
AndreBlankholm <ablankholm@gmail.com>
2023-01-06 14:30:20 -0600

    adding listening port

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Animal-Keeper
+## https://animal-keeper.herokuapp.com/ 
+## https://git.heroku.com/animal-keeper.git
---
+++ server.js
@@ -0,0 +1,76 @@
+const { animals } = require("./data/animals.json");
+
+// instantiates the server
+const express = require("express");
+
+// When Heroku runs the app, we teell the app to use this port if it has been set and if not, set to 3001
+const PORT = process.env.PORT || 3001;
+//server to listen for requests
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+////////////////////////////////////////////////////////////////////////////
+
+function filterByQuery(query, animalsArray) {
+
+  let personalityTraitsArray = [];
+  // Note that we save the animalsArray as filteredResults here:
+  let filteredResults = animalsArray;
+
+
+  if (query.personalityTraits) {
+    // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // Loop through each trait in the personalityTraits array:
+    personalityTraitsArray.forEach((trait) => {
+      // Check the trait against each animal in the filteredResults array.
+      // Remember, it is initially a copy of the animalsArray,
+      // but here we're updating it for each trait in the .forEach() loop.
+      // For each trait being targeted by the filter, the filteredResults
+      // array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one
+      // of the traits when the .forEach() loop is finished.
+      filteredResults = filteredResults.filter(
+                        // returns an index if true
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  // return the filtered results:
+  return filteredResults;
+}
+
+//request-----get requires 2 arugments/ 1) the string that describes the route, 2)the callback evreytime
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+////////////////////////////////////////////////////////////////////////////
+//method to listen for requests
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
